29c3cc30898ab995f814141817de8bee4a0782b7
Fernanda Conde <fernanda-conde@hotmail.com>
2021-02-17 11:59:32 -0300

    testing with junit

---
+++ src/test/java/com/alura/forum/repository/CursoRepositoryTest.java
@@ -0,0 +1,47 @@
+package com.alura.forum.repository;
+
+import com.alura.forum.model.Curso;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@ActiveProfiles("test")
+public class CursoRepositoryTest {
+
+    @Autowired
+    private CursoRepository repository;
+
+    @Autowired
+    private TestEntityManager em;
+
+    @Test
+    public void deveriaCarregarUmCursoPeloNome() {
+        String nomeCurso = "HTML 5";
+
+        Curso html5 = new Curso();
+        html5.setNome(nomeCurso);
+        html5.setCategoria("Programacao");
+        em.persist(html5);
+
+        Curso curso = repository.findByNome(nomeCurso);
+        Assert.assertNotNull(curso);
+        Assert.assertEquals(nomeCurso, curso.getNome());
+    }
+
+    @Test
+    public void naoDeveriaCarregarUmCursoNaoCadastrado() {
+        String nomeCurso = "JPA";
+        Curso curso = repository.findByNome(nomeCurso);
+
+        Assert.assertNull(curso);
+    }
+}
